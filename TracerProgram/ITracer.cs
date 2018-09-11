4a7397814ef67f67d614a2812387390203706d96
DenisKaminsky <deniskaminsky123@mail.ru>
2018-09-11 18:43:25 +0300

    second commit

--- TracerProgram/ITracer.cs
+++ TracerProgram/ITracer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TracerProgram
+﻿namespace TracerProgram
 {
     public interface ITracer
     {
@@ -12,7 +6,6 @@ namespace TracerProgram
 
         void StopTrace();
 
-
-
+        TraceResult GetTraceResult();
     }
 }
--- TracerProgram/Tracer.cs
+++ TracerProgram/Tracer.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace TracerProgram
 {
     public class Tracer : ITracer
     {
+        private Stopwatch stopwatch;
+
         public void StartTrace()
         {
-
+            
         }
 
         public void StopTrace()
         {
 
+        }
+
+        public TraceResult GetTraceResult()
+        {
+
+            return null;
         }
     }
 }
