99779889a06b2617fd45c0793c61bbda8f1be78c
DenisKaminsky <deniskaminsky123@mail.ru>
2018-09-19 00:12:34 +0300

    ConsoleWriter changed

--- TracerProgram/ConsoleWriter.cs
+++ TracerProgram/ConsoleWriter.cs
@@ -1,29 +1,15 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.Serialization.Json;
-using System.Xml;
-using System.Text;
 
 namespace TracerProgram
 {
     public class ConsoleWriter: ITraceWriter
-    {/*
-        private DataContractJsonSerializer jsonFormatter;
-
-        public ConsoleWriter()
-        {
-            jsonFormatter = new DataContractJsonSerializer(typeof(TraceResult));
-        }*/
-
+    {
         public void Write(TraceResult traceresult, ITraceConverter converter)
         {
             using (Stream consolestream = Console.OpenStandardOutput())
             {
                 converter.Convert(traceresult, consolestream);
-                /*using (XmlDictionaryWriter jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(consolestream, Encoding.UTF8, ownsStream: true, indent: true, indentChars: "     "))
-                {
-                    jsonFormatter.WriteObject(jsonWriter, traceresult);
-                }*/
             }
         }
     }
--- TracerProgram/FileWriter.cs
+++ TracerProgram/FileWriter.cs
@@ -4,9 +4,16 @@ namespace TracerProgram
 {
     public class FileWriter:ITraceWriter
     {
+        private string filename;
+
+        public FileWriter(string filename)
+        {
+            this.filename = filename;
+        }
+
         public void Write(TraceResult result,ITraceConverter converter)
         {
-            using (FileStream fs = new FileStream("json.dat", FileMode.Create))
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
             {
                 converter.Convert(result, fs);
             }
